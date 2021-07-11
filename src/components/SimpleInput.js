01bc474362c69df68bd7669fbcf5fe45eee984da
Mazen <mazenadel19@gmail.com>
2021-07-11 17:52:58 +0200

    getting data with state & ref

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,39 @@
-const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
-};
+import { useRef, useState } from 'react'
 
-export default SimpleInput;
+const SimpleInput = props => {
+	const [enteredName, setEnteredName] = useState('')
+
+	const inputRef = useRef()
+	const btnRef = useRef()
+
+	const inputHandler = e => {
+		setEnteredName(e.target.value) // use state for validation with everykey stroke
+	}
+
+	const submitHandler = e => {
+		e.preventDefault()
+		console.log(inputRef.current.value) // use ref for validation once after form submitting
+    // inputRef.current.value='' //-> not recommended
+		setEnteredName('')
+	}
+
+	return (
+		<form onSubmit={submitHandler}>
+			<div className='form-control'>
+				<label htmlFor='name'>Your Name</label>
+				<input
+					type='text'
+					id='name'
+					ref={inputRef}
+					value={enteredName}
+					onChange={inputHandler}
+				/>
+			</div>
+			<div className='form-actions'>
+				<button ref={btnRef}>Submit</button>
+			</div>
+		</form>
+	)
+}
+
+export default SimpleInput

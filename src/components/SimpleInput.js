056370e5e09add9ebce2eb66328d2e773bd48bc8
Mazen <mazenadel19@gmail.com>
2021-07-12 18:26:22 +0200

    refactor

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,57 +2,82 @@ import { useState } from 'react'
 
 const SimpleInput = props => {
 	const [enteredName, setEnteredName] = useState('')
-	const [enteredNameIsValid, setEnteredNameIsValid] = useState(false)
 	const [enteredNameTouched, setEnteredNameIsTouched] = useState(false)
 
-	const inputHandler = e => {
+	const enteredNameIsValid = enteredName.trim().length > 5
+	const invalid = !enteredNameIsValid && enteredNameTouched
+
+
+	const nameInputChangeHandler = e => {
 		setEnteredName(e.target.value)
-
-		setEnteredNameIsTouched(true)
-
-		if (enteredName.trim().length < 5) {
-			return setEnteredNameIsValid(false)
-		} else {
-			setEnteredNameIsValid(true)
-			console.log(enteredName)
-			console.log(enteredNameIsValid)
-		}
 	}
 
-	const submitHandler = e => {
+	const nameInputBlurHandler = e => {
+		setEnteredNameIsTouched(true)
+	}
+
+	const formSubmitHandler = e => {
 		e.preventDefault()
 
 		setEnteredNameIsTouched(true)
-		setEnteredName('')
+
+		if (!enteredNameIsValid) {
+			return
+		}
+    setEnteredName('')
+    setEnteredNameIsTouched(false)
 	}
-
-	const invalid = !enteredNameIsValid && enteredNameTouched
-
-	console.log(
-		enteredNameIsValid,
-		'enteredNameIsValid',
-		enteredNameTouched,
-		'enteredNameTouched',
-	)
-	console.log(invalid)
 
 	const nameInputClasses = invalid ? 'form-control invalid' : 'form-control'
 
+
+  	console.log(
+			`%centeredNameIsValid ${enteredNameIsValid} && enteredNameTouched ${enteredNameTouched}`,
+			'background: #444; color: #aadaff; padding: 2px; border-radius:2px;',
+		)
+
+		if (invalid) {
+			console.log(
+				`%cInvalid ${invalid}`,
+				'background: #444; color: #da5555; padding: 2px; border-radius:2px;',
+			)
+		} else {
+			console.log(
+				`%cInvalid ${invalid}`,
+				'background: #444; color: #bada55; padding: 2px; border-radius:2px;',
+			)
+		}
+
+		if (enteredName.trim().length > 5) {
+			console.log(
+				`%cEnteredName has ${enteredName.trim().length} characters now`,
+				'background: #444; color: #bada55; padding: 2px; border-radius:2px;',
+			)
+		} else {
+			console.log(
+				`%cEnteredName has ${enteredName.trim().length} characters now`,
+				'background: #444; color: #da5555; padding: 2px; border-radius:2px;',
+			)
+		}
+
+
 	return (
-		<form onSubmit={submitHandler}>
+		<form onSubmit={formSubmitHandler}>
 			<div className={nameInputClasses}>
 				<label htmlFor='name'>Your Name</label>
 				<input
 					type='text'
 					id='name'
 					value={enteredName}
-					onChange={inputHandler}
+					onBlur={nameInputBlurHandler}
+					onChange={nameInputChangeHandler}
+					autoComplete='off'
 				/>
 			</div>
-			{enteredNameIsValid ? 'true' : 'false'}
 			{invalid && (
 				<p className='error-text'>
-					Name Input must be greater than 5 character
+					Name Input must be greater than 5 character, it's{' '}
+					{enteredName.trim().length} characters now
 				</p>
 			)}
 			<div className='form-actions'>

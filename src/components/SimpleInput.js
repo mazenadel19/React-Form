70c7af2bcfa151d91664431ef6c82bafa15a1d66
Mazen <mazenadel19@gmail.com>
2021-07-13 11:44:37 +0200

    better validation without useEffect

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,17 +1,15 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 const SimpleInput = props => {
 	const [enteredName, setEnteredName] = useState('')
 	const [enteredNameTouched, setEnteredNameIsTouched] = useState(false)
-	const [formIsValid, setFormIsValid] = useState(false)
 
 	const enteredNameIsValid = enteredName.trim().length > 5
 	const invalid = !enteredNameIsValid && enteredNameTouched
 
-	useEffect(() => {
-		if (enteredNameIsValid) setFormIsValid(true)
-		else setFormIsValid(false)
-	}, [enteredNameIsValid])
+	let formIsValid=false
+
+	if (enteredNameIsValid) formIsValid = true
 
 	const nameInputChangeHandler = e => {
 		setEnteredName(e.target.value)
